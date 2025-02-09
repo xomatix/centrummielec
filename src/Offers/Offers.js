cee921d17de84253520c01a4e42dfd94f4da6d71
xomatix <omatix2003@interia.pl>
2025-02-09 20:00:15 +0100

    space test

--- src/Offers/Offers.js
+++ src/Offers/Offers.js
@@ -110,7 +110,7 @@ function Offers({ rent, mielec }) {
   return (
     <div className="" style={{ background: "#F2F3F4" }}>
       {loading ? (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center  items-center h-screen">
           <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
         </div>
       ) : (
